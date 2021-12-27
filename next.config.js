8899547109d58ffd7d62731a77b205cb779285d2
Bob Engel <rae004@gmail.com>
2021-12-27 16:10:34 -0500

    removing commented code.

--- next.config.js
+++ next.config.js
@@ -1,15 +1,3 @@
-// import fs from 'fs';
-// import dotenv from 'dotenv';
-//
-// const envPath = '../.env';
-// const localEnvPath = '../.env.local';
-//
-// if (fs.existsSync(`${localEnvPath}`)) {
-//     dotenv.config({ path: `${localEnvPath}` });
-// } else {
-//     dotenv.config({ path: `${envPath}` });
-// }
-
 /** @type {import('next').NextConfig} */
 module.exports = {
     swcMinify: true,
